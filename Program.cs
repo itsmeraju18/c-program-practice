69f0faf614dbfe384ee0997f8814015d182ebd94
itsmeraju18 <69288937+itsmeraju18@users.noreply.github.com>
2020-08-06 10:52:22 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[5] { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine(array[0]);
+            Console.WriteLine(array[1]);
+            Console.WriteLine(array[2]);
+            Console.WriteLine(array[3]);
+            Console.WriteLine(array[4]);
+            Console.WriteLine("press enter key to exit");
+            Console.ReadLine();
+
+        }
+    }
+}
